494b04e9370b5dafe7d450ba8436d9478244132e
chau0301 <nongngocchau1@gmail.com>
2023-07-03 22:28:27 +0700

    fix build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import Layout from "../components/Layout";
 import Home from "../components/Home";
 import LoginWithGoogleButton from "../components/LoginWithGoogle";
+import Generate from "../components/Generate";
 
 const HomePage: React.FC = () => {
   return (
     <Layout>
-      <Home/>
-      <LoginWithGoogleButton />
+      <Generate />
+      {/* <LoginWithGoogleButton /> */}
     </Layout>
   );
 };
